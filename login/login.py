5a61ce39df4561e9b091a4127daa42b7dc4f93a9
ricky3325 <rick3325@gmail.com>
2021-06-05 13:20:59 +0800

    add login post

--- login/login.py
+++ login/login.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify, json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
-  username = input()
-  return '%r:Web App with Python Flask!' % username
+  # retrive post body
+  jsonobj = request.get_json(silent=True)
+  username = json.dumps(jsonobj['username']).replace("\"", "")
+  password = json.dumps(jsonobj['password']).replace("\"", "")
+
+  Dmessage = dict()
+  Dmessage['username'] = username
+  Dmessage['password'] = password
+
+  Dmessage = json.dumps(Dmessage)
+
+
+  return ' %r :Web App with Python Flask!' % Dmessage
 
 app.run(host='0.0.0.0', port=5002)
